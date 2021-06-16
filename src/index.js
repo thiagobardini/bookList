22609044b04196f310a8b9001830c4a741644a55
thiagobadini <thiagobardini@hotmail.com>
2021-06-16 17:00:47 -0400

    Book List Project - Review  React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// CSS
+import './index.css'
+//setup  vars
+const books = [
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51bv-74HjAL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+    title: 'Bootstrapping',
+    author: 'Thierry Bardini',
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51Kwaw5nInL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
+    title: 'Learning React: Modern Patterns for Developing React Apps',
+    author: 'Alex Banks',
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51popQ6rqdL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
+    title: 'Easy Origami (Dover Origami Papercraft)over 30 simple projects',
+    author: 'John Bach',
+  },
+]
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (
+    <section className='booklist'>
+      {books.map((book) => {
+        // const { img, title, author } = book
+        return (
+          <Book book={book}></Book>
+          // <div>
+          //   <img src={img} alt='' />
+          //   <h3>{title}</h3>
+          //   <h6>{author}</h6>
+          // </div>
+        )
+      })}
+    </section>
+  )
+}
+
+// const Book = ({ img, title, author }) => {
+const Book = (props) => {
+  const { img, title, author } = props.book
+  return (
+    <article className='book'>
+      <img src={img} alt='' />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  )
+}
+
+// const Greeting = () => {
+//   return React.createElement('div', React.createElement('h1', {}, 'hello word'))
+// }
+
+ReactDom.render(<BookList />, document.getElementById('root'))
