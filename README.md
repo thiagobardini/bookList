4f1c31c3919ef4c6624d6e41b4930a132e39e632
thiagobadini <thiagobardini@hotmail.com>
2021-06-17 10:23:31 -0400

    Added SyntheticEvent, attribute, and eventHandler

--- README.md
+++ README.md
@@ -2,3 +2,29 @@
 
 - create a file `.env`
 - add `FAST_REFRESH=false`
+
+## Supported Events
+
+React normalizes events so that they have consistent properties across different browsers.
+
+The event handlers below are triggered by an event in the bubbling phase. To register an event handler for the capture phase, append `Capture` to the event name; for example, instead of using `onClick`, you would use `onClickCapture` to handle the click event in the capture phase.
+
+- Clipboard Events
+- Composition Events
+- Keyboard Events
+- Focus Events
+- Form Events
+- Generic Events
+- Mouse Events
+- Pointer Events
+- Selection Events
+- Touch Events
+- UI Events
+- Wheel Events
+- Media Events
+- Image Events
+- Animation Events
+- Transition Events
+- Other Events
+
+[SyntheticEvent - Reference Site](https://reactjs.org/docs/events.html)
--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import './index.css'
 //setup  vars
 const books = [
   {
+    id: 1,
     img: 'https://images-na.ssl-images-amazon.com/images/I/51bv-74HjAL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
     title: 'Bootstrapping',
     author: 'Thierry Bardini',
   },
   {
+    id: 2,
     img: 'https://images-na.ssl-images-amazon.com/images/I/51Kwaw5nInL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
     title: 'Learning React: Modern Patterns for Developing React Apps',
     author: 'Alex Banks',
   },
   {
+    id: 3,
     img: 'https://images-na.ssl-images-amazon.com/images/I/51popQ6rqdL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
     title: 'Easy Origami (Dover Origami Papercraft)over 30 simple projects',
     author: 'John Bach',
@@ -28,7 +31,9 @@ function BookList() {
       {books.map((book) => {
         // const { img, title, author } = book
         return (
-          <Book book={book}></Book>
+          <Book key={book.id} {...book}></Book>
+          // <Book key={book.id} book={book}></Book> //  above using spread
+
           // <div>
           //   <img src={img} alt='' />
           //   <h3>{title}</h3>
@@ -40,20 +45,33 @@ function BookList() {
   )
 }
 
-// const Book = ({ img, title, author }) => {
-const Book = (props) => {
-  const { img, title, author } = props.book
+// atribute, eventHandler
+// onClick, onMouseOver = Examples
+
+const Book = ({ img, title, author }) => {
+  // const Book = (props) => {
+  //   const { img, title, author } = props.book
+  const clickHandler = (e) => {
+    console.log(e)
+    console.log(e.target)
+    alert('Hello Word')
+  }
+  const complexExample = (author) => {
+    console.log(author)
+  }
   return (
-    <article className='book'>
+    <article className='book' onMouseOver={() => console.log(title)}>
       <img src={img} alt='' />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
+      <button type='button' onClick={clickHandler}>
+        reference example
+      </button>
+      <button type='button' onClick={() => complexExample(author)}>
+        more complex examples
+      </button>
     </article>
   )
 }
-
-// const Greeting = () => {
-//   return React.createElement('div', React.createElement('h1', {}, 'hello word'))
-// }
 
 ReactDom.render(<BookList />, document.getElementById('root'))
